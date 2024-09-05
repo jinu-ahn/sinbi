fdc1ff792a2754342ce8521da86b4dc0797d9b8e
이학현 <gkrgus008@nate.com>
2024-09-05 11:44:48 +0900

    fix: Repository JPA 상속(Integer에서 Long으로 변경)

--- backend/src/main/java/c104/sinbi/domain/receiver/repository/ReceiverRepository.java
+++ backend/src/main/java/c104/sinbi/domain/receiver/repository/ReceiverRepository.java
@@ -3,5 +3,5 @@ package c104.sinbi.domain.receiver.repository;
 import c104.sinbi.domain.receiver.Receiver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReceiverRepository extends JpaRepository<Receiver, Integer> {
+public interface ReceiverRepository extends JpaRepository<Receiver, Long> {
 }
--- backend/src/main/java/c104/sinbi/domain/virtualaccount/repository/VirtualAccountRepository.java
+++ backend/src/main/java/c104/sinbi/domain/virtualaccount/repository/VirtualAccountRepository.java
@@ -3,5 +3,5 @@ package c104.sinbi.domain.virtualaccount.repository;
 import c104.sinbi.domain.virtualaccount.VirtualAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VirtualAccountRepository extends JpaRepository<VirtualAccount, Integer> {
+public interface VirtualAccountRepository extends JpaRepository<VirtualAccount, Long> {
 }
