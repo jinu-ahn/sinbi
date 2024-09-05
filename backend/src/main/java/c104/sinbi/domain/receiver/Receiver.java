1125ff0d4011ad592d5ed689cc5530ec5d371b04
이학현 <gkrgus008@nate.com>
2024-09-05 10:46:50 +0900

    feat(자주 이체할 계좌): 자주 이체할 계좌 파일구조,테이블 설계

---
+++ backend/src/main/java/c104/sinbi/domain/receiver/Receiver.java
@@ -0,0 +1,26 @@
+package c104.sinbi.domain.receiver;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Receiver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recv_id")
+    private Long id;
+
+    @Column(name = "recv_name")
+    private String recvName;
+
+    @Column(name = "recv_account_num")
+    private String recvAccountNum;
+
+    @Column(name = "recv_alias")
+    private String recvAlias;
+}
