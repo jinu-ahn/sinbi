65fdd34b6c9f9077694055497fa2f839f1f37c59
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-09-05 13:25:52 +0900

    feat(은행타입): 은행 타입 설정

--- backend/src/main/java/c104/sinbi/common/constant/BankTypeEnum.java
+++ backend/src/main/java/c104/sinbi/common/constant/BankTypeEnum.java
@@ -1,4 +1,11 @@
 package c104.sinbi.common.constant;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public enum BankTypeEnum {
+    KB, SHINHAN, NH, IBK, HANA, WOORI, IM, KAKAO, TOSS, SINBI;
+
 }
