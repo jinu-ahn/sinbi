e0c2b2991f3405572e83e94c65f894e9e13a27fc
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-09-07 17:38:38 +0900

    feat(회원가입): 유저 회원가입 구현

--- backend/src/main/java/c104/sinbi/domain/user/controller/UserController.java
+++ backend/src/main/java/c104/sinbi/domain/user/controller/UserController.java
@@ -1,11 +1,25 @@
 package c104.sinbi.domain.user.controller;
 
+import c104.sinbi.common.ApiResponse;
+import c104.sinbi.domain.user.dto.SignUpDto;
+import c104.sinbi.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<String>> signUp(@RequestPart(value="signUpDto") SignUpDto signUpDto,
+                                                      @RequestPart(value= "image") MultipartFile multipartFile){
+        userService.signup(signUpDto,multipartFile);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success("SUCCESS"));
+    }
 }
--- backend/src/main/java/c104/sinbi/domain/user/service/UserService.java
+++ backend/src/main/java/c104/sinbi/domain/user/service/UserService.java
@@ -1,11 +1,34 @@
 package c104.sinbi.domain.user.service;
 
+import c104.sinbi.common.config.s3.S3Uploader;
+import c104.sinbi.common.exception.UserAlreadyExistsException;
+import c104.sinbi.domain.user.User;
+import c104.sinbi.domain.user.dto.SignUpDto;
+import c104.sinbi.domain.user.repository.UserRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserService {
+    private final UserRepository userRepository;
+    // TODO : Security 에서 Bean 등록 후 new BCryptPasswordEncoder 삭제 예정
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final S3Uploader s3Uploader;
+    @Transactional
+    public void signup(@Valid final SignUpDto signUpDto, final MultipartFile multiPartFile) {
+        String encodedPassword = passwordEncoder.encode(signUpDto.getUserPassword()); // 패스워드 인코딩
+        String convertImageUrl = null;
+        if(!multiPartFile.isEmpty())
+            convertImageUrl = s3Uploader.putS3(multiPartFile); // 얼굴인증 이미지 S3 저장
+        User user = User.builder().signUpDto(signUpDto).encodedPassword(encodedPassword).convertImageUrl(convertImageUrl).build();
+
+        userRepository.save(user);
+    }
 }
