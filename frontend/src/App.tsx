90d907085f84b4b530e9b25d97d9907918e8e77e
이정하 <dlchris19@gmail.com>
2024-09-09 18:00:09 +0900

    feat(메인페이지): 메인페이지 버튼 반응형 구현
    - tailwind 커스텀 해둔 width 따라 폰트 사이즈 조정
    - 버튼 사이 gap 조정
    - 버튼 속 text는 string[]로 변경, 줄바꿈 가능하도록
    
    ISSUE ID: S11P21C104-126

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,28 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
-import YellowBox from "./components/YellowBox";
-import YellowButton from "./components/YellowButton";
+// import YellowBox from "./components/YellowBox";
+// import YellowButton from "./components/YellowButton";
+import MainPage from "./MainPage";
 
-function App() {
+// function App() {
+const App: React.FC = () => {
   return (
-    <div className="bg-white flex flex-col items-center justify-center h-screen">
-      <YellowBox>
-        <p>hello</p>
-        <input type="text" placeholder="Enter something here" />
-      </YellowBox>
-      <YellowButton height={70} width={80}>
-        <p className="font-bold">계좌 등록</p>
-      </YellowButton>
-    </div>
+    // <div className="bg-white flex flex-col items-center justify-center h-screen">
+    //   <YellowBox>
+    //     <p>hello</p>
+    //     <input type="text" placeholder="Enter something here" />
+    //   </YellowBox>
+    //   <YellowButton height={70} width={80}>
+    //     <p className="font-bold">계좌 등록</p>
+    //   </YellowButton>
+    // </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<MainPage />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
