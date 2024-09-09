2962916d7842a9d12a808042af0a95bf0a44b1cf
이학현 <gkrgus008@nate.com>
2024-09-10 00:03:27 +0900

    feat(상대방 계좌 조회) : 상대방 계좌 조회 기능 구현

--- backend/src/main/java/c104/sinbi/domain/virtualaccount/service/VirtualAccountService.java
+++ backend/src/main/java/c104/sinbi/domain/virtualaccount/service/VirtualAccountService.java
@@ -1,11 +1,30 @@
 package c104.sinbi.domain.virtualaccount.service;
 
+import c104.sinbi.common.exception.AccountNotFoundException;
+import c104.sinbi.domain.virtualaccount.VirtualAccount;
+import c104.sinbi.domain.virtualaccount.dto.VirtualAccountCheckRequest;
+import c104.sinbi.domain.virtualaccount.repository.VirtualAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class VirtualAccountService {
+
+    private final VirtualAccountRepository virtualAccountRepository;
+
+    //상대방 계좌 조회
+    public void VirtualAccountCheck(VirtualAccountCheckRequest virtualAccountCheckRequest) {
+        Optional<VirtualAccount> virtualAccountCheck = virtualAccountRepository.findByAccountNumAndBankType(
+                virtualAccountCheckRequest.getAccountNum(),
+                virtualAccountCheckRequest.getBankTypeEnum());
+
+        if (virtualAccountCheck.isEmpty()) {
+            throw new AccountNotFoundException();
+        }
+    }
 }
