ea8dafcc84c318346ed5b21b2cec04ede8cb9e42
heejoo <heejookim99@gmail.com>
2024-09-10 14:20:05 +0900

    chore (확인하려고 만든 노란박스&버튼 컴포넌트 App.tsx에서 삭제)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,6 @@ import MainPage from "./MainPage";
 // function App() {
 const App: React.FC = () => {
   return (
-    // <div className="bg-white flex flex-col items-center justify-center h-screen">
-    //   <YellowBox>
-    //     <p>hello</p>
-    //     <input type="text" placeholder="Enter something here" />
-    //   </YellowBox>
-    //   <YellowButton height={70} width={80}>
-    //     <p className="font-bold">계좌 등록</p>
-    //   </YellowButton>
-    // </div>
     <BrowserRouter>
       <div>
         <Routes>
