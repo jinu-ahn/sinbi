0da4f1ced243eb96377d223194a3e161268f3ec1
이학현 <gkrgus008@nate.com>
2024-09-10 11:20:58 +0900

    feat(자주 사용할 계좌 목록 보기) : 자주 사용할 계좌 목록 보기 기능 구현

--- backend/src/main/java/c104/sinbi/domain/receiver/controller/ReceiverController.java
+++ backend/src/main/java/c104/sinbi/domain/receiver/controller/ReceiverController.java
@@ -1,18 +1,18 @@
 package c104.sinbi.domain.receiver.controller;
 
 import c104.sinbi.common.ApiResponse;
+import c104.sinbi.domain.receiver.dto.ReceiverAccountListResponse;
 import c104.sinbi.domain.receiver.dto.ReceiverRegistrationRequest;
 import c104.sinbi.domain.receiver.service.ReceiverService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/receiverAccount")
+@RequestMapping("/receiverAccount")
 public class ReceiverController {
 
     private final ReceiverService receiverService;
@@ -22,5 +22,12 @@ public class ReceiverController {
     public ResponseEntity<ApiResponse<?>> ReceiverAccountRegistration(@RequestBody ReceiverRegistrationRequest receiverRegistrationRequest){
         receiverService.ReceiverAccountRegistration(receiverRegistrationRequest);
         return ResponseEntity.ok(ApiResponse.success("자주 사용할 계좌 등록이 완료 되었습니다."));
+    }
+
+    //자주 사용할 계좌 목록 보기
+    @GetMapping("/list")
+    public ResponseEntity<ApiResponse<?>> ReceiverAccountList(@RequestParam int userId){
+        List<ReceiverAccountListResponse> receiverAccountListResponses = receiverService.receiverAccountList(userId);
+        return ResponseEntity.ok(ApiResponse.success(receiverAccountListResponses,"자주 사용할 계좌 목록 불러오기 완료"));
     }
 }
