16aca8358bdb6445d6832c4f5251473ae06c984d
이정하 <dlchris19@gmail.com>
2024-09-20 16:10:43 +0900

    fix(공통컴포넌트): YellowButton 컴포넌트에 onClick props 추가

--- frontend/src/components/YellowButton.tsx
+++ frontend/src/components/YellowButton.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { YellowButtonProps } from "./CommonComponent.types";
 
-const YellowButton: React.FC<YellowButtonProps> = ({ children, height, width }) => {
-  return <button style={{ height: `${height}px`, width: `${width}px` }} className={`bg-[#FFC700] p-4 rounded-md`}>{children}</button>;
+const YellowButton: React.FC<YellowButtonProps> = ({ children, height, width, onClick }) => {
+  return <button onClick={onClick} style={{ height: `${height}px`, width: `${width}px` }} className={`bg-[#FFC700] p-4 rounded-md`}>{children}</button>;
 };
 
 export default YellowButton;
