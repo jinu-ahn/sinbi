ba33e6fe03d767d0a173094a537367e143c9213f
이정하 <dlchris19@gmail.com>
2024-10-10 13:31:44 +0900

    fix:2초후 sim으로

--- frontend/src/features/User/SignUpStep/FaceRecognitionCompleteStep.tsx
+++ frontend/src/features/User/SignUpStep/FaceRecognitionCompleteStep.tsx
@@ -1,14 +1,27 @@
 // src/components/User/SignUpStep/FaceRecognitionCompleteStep.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import GreenText from "../../../components/GreenText";
+import { useNavigate } from 'react-router-dom';
 // import useUserStore from '../useUserStore';
 
 // interface FaceRecognitionCompleteStepProps {
 //   onComplete: () => void;
 // }
 
+
 const FaceRecognitionCompleteStep: React.FC = () => {
   // const {nextStep} = useUserStore()
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 2초 후에 '/sim'으로 이동
+    const timer = setTimeout(() => {
+      navigate('/sim');
+    }, 2000);
+
+    // 컴포넌트가 언마운트되면 타이머를 정리
+    return () => clearTimeout(timer);
+  }, [navigate]);
   return (
     <>
       <GreenText text="얼굴 인식이 완료되었습니다." boldChars={["완료"]} />
